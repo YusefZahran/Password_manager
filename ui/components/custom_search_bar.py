49110dccb260771399762daab4b3df8bad23d218
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2023-08-01 19:57:49 +0300

    Update custom_search_bar.py

--- ui/components/custom_search_bar.py
+++ ui/components/custom_search_bar.py
@@ -8,7 +8,7 @@ from ui.components.custom_entry import CustomEntry
 class CustomSearchBar:
     """Custom Search Bar. Combines CustomEntry and CustomButton to create a custom search bar"""
 
-    def __init__(self, master: tk.Misc, text_variable: tk.Variable = None, search_button_text="Search",
+    def __init__(self, master: tk.Misc, text_variable: tk.Variable = None, command=None, search_button_text="Search",
                  separation: int = -95, x: int = 0, y: int = 0):
         """
         Custom Search bar constructor
@@ -21,7 +21,14 @@ class CustomSearchBar:
         """
         x_offset = x - separation
 
-        CustomEntry(master, text_variable, x=x, y=y)
+        CustomEntry(master,
+                    text_variable,
+                    x=x,
+                    y=y)
 
-        CustomButton(master, search_button_text, button_size=globals.SMALL_BUTTON_SIZE,
-                     x=x_offset, y=y)
+        CustomButton(master,
+                     search_button_text,
+                     button_size=globals.SMALL_BUTTON_SIZE,
+                     command=command,
+                     x=x_offset,
+                     y=y)
