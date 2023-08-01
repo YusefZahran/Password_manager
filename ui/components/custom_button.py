5f2b77fbdbafe042cd873b90198866d6926fb1c6
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2023-08-01 12:36:30 +0300

    Add command to custom_button

--- ui/components/custom_button.py
+++ ui/components/custom_button.py
@@ -6,11 +6,12 @@ import globals
 class CustomButton(tk.Button):
     """Custom Button class. Derived from Tkinter Button"""
 
-    def __init__(self, master: tk.Misc, text: str = None, x: int = 0, y: int = 0):
+    def __init__(self, master: tk.Misc, text: str = None, command=None, x: int = 0, y: int = 0):
         """
         Custom Button constructor
         :param master: The master (parent) component for the entry to be relative to
         :param text: The text to show on the button
+        :param command: The command to execute when the button is clicked
         :param x: The x position
         :param y: The y position
         """
@@ -18,6 +19,7 @@ class CustomButton(tk.Button):
                          text=text,
                          font=('calibre', 7, 'normal'),
                          justify="center",
+                         command=command,
                          bg=globals.BUTTON_BG_COLOR,
                          fg="#FFFFFF")
         self.place(x=x, y=y, anchor=tk.CENTER)
