02e0d139a6add13eb1d6fe856f6f97d169a7258b
Youssef Zahran <yusef.a.zahran@gmail.com>
2023-08-02 15:06:34 +0300

    Create show_password_form.py

---
+++ ui/frames/show_password_form.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+from ui.components.custom_vertical_input_field import CustomVerticalInputField
+from ui.frames.custom_frame import CustomFrame
+
+
+class ShowPasswordForm:
+    """Show Password Form Frame"""
+
+
+ShowPasswordForm()
