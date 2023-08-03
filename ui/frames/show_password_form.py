49a1cbc8b2a61965ea0f3cb87549091cbc111c90
Youssef Zahran <yusef.a.zahran@gmail.com>
2023-08-03 14:51:44 +0300

    Update show_password_form.py

--- ui/frames/show_password_form.py
+++ ui/frames/show_password_form.py
@@ -1,21 +1,25 @@
 import tkinter as tk
 from Password import Account
+import string
 
 # from ui.components.custom_vertical_input_field import CustomVerticalInputField
 from ui.frames.custom_frame import CustomFrame
+from ui.components.custom_label import CustomLabel
 
 
 class ShowPasswordForm(CustomFrame):
     """Show Password Form Frame"""
     # region properties
-    title_var: tk.StringVar
-    username_var: tk.StringVar
-    password_var: tk.StringVar
-    details_var: tk.StringVar
+    title_var: string
+    username_var: string
+    password_var: string
+    details_var: string
     # endregion
 
-def __init__(self, master: tk.Misc,account: Account):
-    title_var = account.title
-    username_var = account.username
-    password_var = account.get_password()
-    #details = 
+    def __init__(self, master: tk.Misc,account: Account):
+        title_var = account.title
+        username_var = account.username
+        password_var = account.get_password()
+        # details = account.details
+        t_label = CustomLabel(self, master, text=title_var, x=50, y=50)
+        super.__init__(super, master)
